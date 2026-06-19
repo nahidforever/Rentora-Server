21451aa000275035ba82f37404fcf76c19bc9094
Nahid1828 <isam15-5334@diu.edu.bd>
2026-06-19 08:23:38 +0600

    Setup backend & connected to MongoDB, Also implement add property API

---
+++ index.js
@@ -0,0 +1,70 @@
+const dns = require("node:dns");
+dns.setServers(["1.1.1.1", "1.0.0.1"]);
+
+const express = require("express");
+const dontenv = require("dotenv");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+dontenv.config();
+
+const uri = process.env.MONGODB_URI;
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(
+  cors({
+    credentials: true,
+    origin: [process.env.CLIENT_URL],
+  }),
+);
+app.use(express.json());
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db("rentora-db");
+    const userCollection = db.collection("user");
+    const propertyCollection = db.collection("property");
+
+    app.post("/owner/property", async (req, res) => {
+      try {
+        const property = req.body;
+
+        const result = await propertyCollection.insertOne({
+          ...property,
+          createdAt: new Date(),
+        });
+
+        res.send(result);
+      } catch (error) {
+        res.status(500).send({ error: "Failed to insert property" });
+      }
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Server is running fine!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
